d2b7d55eca757bedba86cd75d19ae7fc7f1d6ddb
agyesam12 <agyesam12@gmail.com>
2025-09-20 14:42:14 +0000

    updated the settings.py to configure secret keys in env file

--- listogodblessshopsystem/settings.py
+++ listogodblessshopsystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^o=2c^_o^$q4kosopdevwp8!---^4(2*=o=c08#7hobf3085(c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
